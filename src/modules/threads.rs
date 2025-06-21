f68c43e06204a64f922dbff1b3c08e09fe5507d7
Akinator31 <pavel.de-wavrechin@epitech.eu>
2025-06-21 16:18:54 +0200

    Fix JOINED field showing Unknown when user is on community server but not staff server

--- src/modules/threads.rs
+++ src/modules/threads.rs
@@ -1,4 +1,5 @@
 use crate::db::operations::create_thread;
+use crate::db::thread_exists;
 use crate::utils::build_message_from_ticket::build_message_from_ticket;
 use crate::utils::format_duration_since::format_duration_since;
 use crate::utils::format_ticket_message::Sender;
@@ -17,15 +18,36 @@ pub async fn create_channel(ctx: &Context, msg: &Message, config: &Config) {
             return;
         }
     };
+
+    let community_guild_id = GuildId::new(config.bot.get_community_guild_id());
+    if let Err(_) = community_guild_id.member(&ctx.http, msg.author.id).await {
+        let error_msg = crate::i18n::get_translated_message(
+            config,
+            "server.not_in_community",
+            None,
+            Some(msg.author.id),
+            Some(community_guild_id.get()),
+            None
+        ).await;
+        
+        let error = crate::errors::common::validation_failed(&error_msg);
+        if let Some(error_handler) = &config.error_handler {
+            let _ = error_handler.reply_with_error(ctx, msg, &error).await;
+        }
+        return;
+    }
+
     let channel_builder = CreateChannel::new(&msg.author.name)
         .category(ChannelId::new(config.thread.inbox_category_id));
-    let guild_id = GuildId::new(config.bot.guild_id);
-    let member_join_date = match get_member_join_date(ctx, msg, guild_id).await {
+    
+    let community_guild_id = GuildId::new(config.bot.get_community_guild_id());
+    let member_join_date = match get_member_join_date(ctx, msg, community_guild_id).await {
         Some(date) => date,
         None => "Unknown".to_string(),
     };
 
-    if let Ok(channel) = guild_id.create_channel(&ctx.http, channel_builder).await {
+    let staff_guild_id = GuildId::new(config.bot.get_staff_guild_id());
+    if let Ok(channel) = staff_guild_id.create_channel(&ctx.http, channel_builder).await {
         let open_thread_message = format!(
             "ACCOUNT AGE **{}**, ID **{}**\nNICKNAME **{}**, JOINED **{}**",
             format_duration_since(msg.author.id.created_at()),
@@ -62,8 +84,12 @@ pub async fn create_channel(ctx: &Context, msg: &Message, config: &Config) {
                 let _ = msg.channel_id.send_message(&ctx.http, dm_message).await;
                 println!("Thread created successfully");
             }
-            Err(e) => eprintln!("Error creating thread: {}", e),
+            Err(e) => {
+                eprintln!("Error creating thread: {}", e);
+                return;
+            }
         }
+        
         if let Err(e) = send_to_thread(ctx, channel.id, msg, config, false).await {
             eprintln!("Failed to forward message to thread: {:?}", e);
         }
