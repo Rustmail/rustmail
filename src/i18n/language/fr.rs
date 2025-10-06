8d7cd2107a40193c0a1645ecec9e76616c20dbf2
Akinator31 <paveldewavrechin@gmail.com>
2025-10-06 10:18:05 +0200

    fix(command): fixed too lond description for recover command

--- src/i18n/language/fr.rs
+++ src/i18n/language/fr.rs
@@ -724,7 +724,7 @@ pub fn load_french_messages(dict: &mut ErrorDictionary) {
     );
     dict.messages.insert(
         "slash_command.recover_command_description".to_string(),
-        DictionaryMessage::new("Récupérer les messages manqués pendant la période d'indisponibilité du bot (Ce processus est automatique)"),
+        DictionaryMessage::new("Récupérer les messages manqués pendant la période d'indisponibilité du bot (automatique)"),
     );
     dict.messages.insert(
         "slash_command.help_command_description".to_string(),
