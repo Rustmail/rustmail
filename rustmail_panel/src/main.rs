e6b1875ea7a330ff62566c1dc7a28dec206686d0
Akinator31 <paveldewavrechin@gmail.com>
2025-10-14 22:06:25 +0200

    fix(panel): fixed yew page router

--- rustmail_panel/src/main.rs
+++ rustmail_panel/src/main.rs
@@ -2,13 +2,16 @@ mod components;
 mod pages;
 mod router;
 
-use crate::components::home::Home;
 use yew::prelude::*;
+use yew_router::prelude::*;
+use crate::router::{switch, Route};
 
-#[component]
+#[function_component]
 fn App() -> Html {
     html! {
-        <Home />
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
     }
 }
 
--- rustmail_panel/src/router.rs
+++ rustmail_panel/src/router.rs
@@ -1,32 +1,23 @@
+use yew_router::prelude::*;
+use yew::prelude::*;
 use crate::pages::docs::Docs;
 use crate::pages::home::Home;
-use yew::prelude::*;
-use yew_router::prelude::*;
 
-#[derive(Routable, PartialEq, Eq, Clone, Debug)]
+#[derive(Clone, Routable, PartialEq)]
 pub enum Route {
     #[at("/")]
     Home,
     #[at("/docs")]
-    Docs,
+    Documentation,
     #[not_found]
     #[at("/404")]
     NotFound,
 }
 
-#[function_component(App)]
-pub fn app() -> Html {
-    html! {
-        <BrowserRouter>
-            <Switch<Route> render={Switch::render(switch)} />
-        </Router>
-    }
-}
-
-fn switch(route: Route) -> Html {
-    match route {
+pub fn switch(routes: Route) -> Html {
+    match routes {
         Route::Home => html! { <Home /> },
-        Route::Docs => html! { <Docs /> },
-        Route::NotFound => html! { <p>{"Page not found"}</p> },
+        Route::Documentation => html! { <Docs /> },
+        Route::NotFound => html! { <h1>{ "404" }</h1> },
     }
 }
